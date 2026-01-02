b547be0c77542ac368f71bec0d11029335e898e9
gilqamesh <celicaw8@gmail.com>
2026-01-02 11:27:13 +0100

    update curl module, pull builder

--- modules/curl/builder_plugin.cpp
+++ modules/curl/builder_plugin.cpp
@@ -1,28 +1,24 @@
-#include <modules/builder/builder_plugin.h>
-#include <modules/builder/builder_api.h>
-#include <modules/builder/builder.h>
+#include <builder/builder_plugin.h>
+#include <builder/compiler.h>
 
 #include <format>
 
-BUILDER_EXTERN void builder__export_bundle_static(builder_ctx_t* ctx, const builder_api_t* api) {
-    throw std::runtime_error("builder__export_bundle_static isn't implemented");
-}
-
-BUILDER_EXTERN void builder__export_bundle_shared(builder_ctx_t* ctx, const builder_api_t* api) {
+BUILDER_EXTERN void builder__export_libraries(builder_ctx_t* ctx, const builder_api_t* api, bundle_type_t bundle_type) {
     const auto system_curl = std::filesystem::path("/usr/lib64/libcurl.so");
     if (!std::filesystem::exists(system_curl)) {
         throw std::runtime_error(std::format("system curl library '{}' does not exist, TODO: extend logic to install it", system_curl.string()));
     }
 
-    builder_t::materialize_shared_library(ctx, api, "curl.so", { "curl.cpp" }, {});
-    builder_t::reference_shared_library(ctx, api, system_curl);
+    compiler_t::create_shared_library(ctx, api, { "curl.cpp" }, {}, "curl");
+    compiler_t::reference_shared_library(ctx, api, system_curl, "curl-ref.so");
 }
 
-BUILDER_EXTERN void builder__link_module(builder_ctx_t* ctx, const builder_api_t* api) {
-    builder_t::materialize_binary(
-        ctx, api, "cli",
+BUILDER_EXTERN void builder__build_module(builder_ctx_t* ctx, const builder_api_t* api) {
+    compiler_t::create_binary(
+        ctx, api,
         { "cli.cpp" },
         {},
-        { api->get_shared_link_command_line(ctx) }
+        BUNDLE_TYPE_SHARED,
+        "cli"
     );
 }
