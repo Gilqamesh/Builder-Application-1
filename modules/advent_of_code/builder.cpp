81736fb1096b5e9e74e19644ae41181f9804cb84
gilqamesh <celicaw8@gmail.com>
2026-01-12 12:29:19 +0100

    pull builder

--- modules/advent_of_code/builder.cpp
+++ modules/advent_of_code/builder.cpp
@@ -1,5 +1,6 @@
 #include <modules/builder/builder.h>
 #include <modules/builder/compiler/cpp_compiler.h>
+#include <modules/builder/filesystem/filesystem.h>
 
 #include <iostream>
 #include <format>
@@ -8,45 +9,21 @@ BUILDER_EXTERN void builder__export_libraries(const builder_t* builder, library_
 }
 
 BUILDER_EXTERN void builder__import_libraries(const builder_t* builder) {
-    const auto source_dir = builder->src_dir();
-    for (const auto& year_dir : std::filesystem::directory_iterator(source_dir)) {
-        if (!year_dir.is_directory()) {
-            continue ;
+    const auto src_files = filesystem_t::find(builder, filesystem_t::cpp_file, filesystem_t::descend_all);
+    const auto input_files = filesystem_t::find(builder, filesystem_t::filename("input"), filesystem_t::descend_all);
+
+    for (const auto& src_file : src_files) {
+        auto rel = builder->src_dir().relative(src_file);
+        rel.extension("");
+        cpp_compiler_t::create_binary(builder, { src_file }, {}, LIBRARY_TYPE_STATIC, rel);
+    }
+
+    for (const auto& input_file : input_files) {
+        const auto dst = builder->import_dir() / builder->src_dir().relative(input_file);
+        const auto parent = dst.parent();
+        if (!filesystem_t::exists(parent)) {
+            filesystem_t::create_directories(parent);
         }
-
-        const auto year_dir_stem = year_dir.path().stem();
-        for (const auto& problem_dir : std::filesystem::directory_iterator(year_dir)) {
-            const auto problem_dir_stem = problem_dir.path().stem();
-            const auto binary_prefix = year_dir_stem / problem_dir_stem;
-            const auto binary_dir = builder->import_dir() / binary_prefix;
-            if (!std::filesystem::exists(binary_dir)) {
-                std::error_code ec;
-                std::filesystem::create_directories(binary_dir, ec);
-                if (ec) {
-                    throw std::runtime_error(std::format("builder__import_libraries: failed to create binary directory '{}': {}", binary_dir.string(), ec.message()));
-                }
-            }
-
-            for (const auto& entry : std::filesystem::directory_iterator(problem_dir)) {
-                const auto path = entry.path();
-
-                const auto filename = path.filename();
-                if (filename == "input") {
-                    std::cout << std::format("cp {} {}", path.string(), (binary_dir / path.filename()).string()) << std::endl;
-                    std::error_code ec;
-                    std::filesystem::copy(path, binary_dir / filename, ec);
-                    if (ec) {
-                        throw std::runtime_error(std::format("builder__import_libraries: failed to copy input file '{}' to '{}': {}", path.string(), (binary_dir / path.filename()).string(), ec.message()));
-                    }
-                    continue ;
-                }
-
-                if (filename.extension() != ".cpp") {
-                    continue ;
-                }
-
-                cpp_compiler_t::create_binary(builder, { path }, {}, LIBRARY_TYPE_STATIC, binary_prefix / filename.stem());
-            }
-        }
+        filesystem_t::copy(input_file, dst);
     }
 }
